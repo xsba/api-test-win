723b339d9c1f2dcdc878a674181ac5600c9d67bf
Sebastian Bargmann <xsba@bankingcircle.com>
2019-02-04 14:01:44 +0100

    Initial commit

---
+++ api-test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace api_test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            Console.WriteLine($"PID: {System.Diagnostics.Process.GetCurrentProcess().Id}");
+
+            return WebHost
+                .CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+        }
+    }
+}
