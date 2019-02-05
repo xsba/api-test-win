f7dd6001688d75bca65f1df8fcf4248e8587da09
Sebastian Bargmann <xsba@bankingcircle.com>
2019-02-05 09:11:51 +0100

    Root route

---
+++ api-test/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_test.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult<string> Index() 
+        //public IActionResult Index()
+        {
+            var pid = System.Diagnostics.Process.GetCurrentProcess().Id.ToString();
+            return $"PID:{pid}";
+        }
+    }
+}
